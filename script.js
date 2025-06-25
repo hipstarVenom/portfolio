07fb405fe43799a198b02ffbafea75748848b1ba
hipstarVenom <yuvansakthi046@gmail.com>
2025-06-25 09:31:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("contact-form");
   
     form.addEventListener("submit", function (e) {
-      e.preventDefault(); // Prevent actual form submission
+
   
       const name = form.name.value.trim();
       const email = form.email.value.trim();
